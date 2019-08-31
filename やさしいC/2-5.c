b39c4f928abd5f91bdfd5e0b3f38e57b0f61a243
Yusyoku Kourou <pocskyscraper@gmail.com>
2019-08-31 12:15:45 +0900

    やさしいC第2章

---
+++ やさしいC/2-5.c
@@ -0,0 +1,20 @@
+//次のように画面に出力するコードを記述してください。
+//8進数、16進数の2通りで出力してください。
+//
+//6
+//20
+//13
+
+#include <stdio.h>
+
+int main(){
+    printf("%d\n", 06);
+    printf("%d\n", 024);
+    printf("%d\n", 015);
+
+    printf("%d\n", 0x6);
+    printf("%d\n", 0x14);
+    printf("%d\n", 0xD);
+
+    return 0;
+}
