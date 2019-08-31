81191787b1002673d73a695bb135e943dd9f6943
Yusyoku Kourou <pocskyscraper@gmail.com>
2019-08-31 12:38:42 +0900

    やさしいC第3章

---
+++ やさしいC/第3章_変数/3-4.c
@@ -0,0 +1,26 @@
+//次のように画面に出力するコードを記述してください。
+//身長を入力してください。
+//165.2(ユーザ入力)
+//体重を入力してください。
+//52.5(ユーザ入力)
+//身長は165.200000センチです。
+//体重は52.500000キロです。
+
+#include <stdio.h>
+
+int main(){
+    printf("身長を入力してください。\n");
+    
+    double height = 0.0;
+    scanf("%lf", &height);
+
+    printf("体重を入力してください。\n");
+    
+    double weight = 0.0;
+    scanf("%lf", &weight);
+
+    printf("身長は%fセンチです。\n", height);
+    printf("体重は%fキロです。", weight);
+
+    return 0;
+}
---
+++ やさしいC/第3章_変数/3-5.c
@@ -0,0 +1,21 @@
+//次のように画面に出力するコードを記述してください。
+//身長と体重を入力してください。
+//165.2(ユーザ入力)
+//52.5(ユーザ入力)
+//身長は165.200000センチ：体重は52.500000キロです。
+
+#include <stdio.h>
+
+int main(){
+    printf("身長と体重を入力してください。\n");
+    
+    double height = 0.0;
+    scanf("%lf", &height);
+
+    double weight = 0.0;
+    scanf("%lf", &weight);
+
+    printf("身長は%fセンチ：体重は%fキロです。\n", height, weight);
+    
+    return 0;
+}
